d871a993313feeafd924b4ca271e76e66c6e8ec9
andriyPro100 <andriy.anatolich@gmail.com>
2021-02-07 13:56:24 +0200

    new components

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,47 +2,21 @@ import React from "react";
 
 import '../styles/Content.css';
 
-import avatar from "../images/avatar.jpg"
 import head_logo from "../images/cloud.jpg"
+import Wall from "./Wall/Wall";
+import Posts from "./Wall/Posts";
+import UserInformation from "./UserInformation";
 
 const Content = () => {
     return (
         <div className="content-wrapper">
-                <div className="head__img">
-                    <img src={head_logo} alt=""/>
-                </div>
-                <div className="content">
-                    <img src={avatar} alt="avatar"/>
-                    <div className="content__information">
-                        <h2 className="information__name">Vasyl Neanderthal</h2>
-                        <div className="information__birth">2 january</div>
-                        <div className="information__city">Kyiv</div>
-                        <div className="information__sex">Male</div>
-                    </div>
-                </div>
-                <div className="wall">
-                    <h2 className="wall__main_text">My posts</h2>
-                    <input type="text" className="wall__input"/>
-                    <div className="wall__button">
-                        <button className="button">Send</button>
-                    </div>
-                </div>
-                <div className="posts-wrapper">
-                    <div className="post">
-                        <img src={avatar} alt=""/>
-                        <div className="post__text">Lorem ipsum dolor sit amet.</div>
-                    </div>
-                    <div className="post">
-                        <img src={avatar} alt=""/>
-                        <div className="post__text">Lorem ipsum dolor sit amet.</div>
-                    </div>
-                    <div className="post">
-                        <img src={avatar} alt=""/>
-                        <div className="post__text">Lorem ipsum dolor sit amet.</div>
-                    </div>
-                </div>
+            <div className="head__img">
+                <img src={head_logo} alt=""/>
             </div>
-
+            <UserInformation/>
+            <Wall/>
+            <Posts/>
+        </div>
     );
 }
 
